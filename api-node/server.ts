d8fe94cfc8075d846b5dc13d8e06d5becdd03d97
ArthurSGO <arthursg33@gmail.com>
2025-06-10 21:51:32 -0300

    feat: adicionar a base do servidor do node

---
+++ api-node/server.ts
@@ -0,0 +1,46 @@
+import express, {Request, Response, NextFunction} from 'express'
+import cors from 'cors'
+import routes from './routes'
+import { ZodError } from 'zod'
+
+const app = express()
+app.use(cors())
+
+app.use(express.json())
+
+const PORT = 3001
+
+app.use(routes)
+
+app.use((
+    error: Error,
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    if (res.headersSent){
+        return next(error);
+    }
+
+    if (error instanceof ZodError){
+        res.status(400)
+        .send({
+            message: "Erro de validação",
+            issues: error.format()
+        })
+
+        return
+    }
+
+    console.log(error)
+    res.status(500).json({
+        status: "erro",
+        message: "Erro interno no servidor"
+    })
+
+    return
+})
+
+app.listen(PORT, () => {
+    console.log('Express iniciou na porta: ' + PORT)
+})
