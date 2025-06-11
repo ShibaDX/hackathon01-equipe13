679faf71212cd52cd92c379686980d6f74b5213a
Kaiky <kaikymassaki2005@gmail.com>
2025-06-11 13:29:37 -0300

    feat: adicionar classe Alunos e ApiNodeService

---
+++ frontend-php/classes/Alunos.php
@@ -0,0 +1,74 @@
+<?php
+
+require_once 'ApiNodeService.php';
+
+class Alunos
+{
+
+    public function __construct()
+    {
+        //$this->db = (new Database())->connect();
+    }
+
+    public function criarAluno($nome, $email, $senha, $telefone, $cpf)
+    {
+        $api = new ApiNodeService();
+        $senha = password_hash($senha, PASSWORD_BCRYPT);
+
+        $aluno = [
+            'nome' => $nome,
+            'email' => $email,
+            'senha' => $senha,
+            'telefone' => $telefone,
+            'cpf' => $cpf
+        ];
+
+        $resposta = $api->cadastrarAluno($aluno);
+
+        if ($resposta['code'] === 201) {
+            echo "Aluno cadastrado com sucesso.";
+        } else {
+            echo "Erro ao cadastrar aluno. Código: " . $resposta['code'];
+            print_r($resposta['body']); // para debug
+        }
+    }
+
+  /*  public function validarLogin($email, $senha)
+    {
+        $sql = "SELECT id, nome, senha from usuarios WHERE email = :email";
+
+        $query = $this->db->prepare($sql);
+        $query->execute(['email' => $email]);
+
+        $usuario = $query->fetch(PDO::FETCH_ASSOC);
+
+        if ($usuario && password_verify($senha, $usuario['senha'])) {
+            return $usuario;
+        }
+
+        return false;
+    } */
+
+    public function buscarAluno(int $id)
+    {
+        $api = new ApiNodeService();
+        $aluno = $api->buscarAluno($id);
+
+        if ($aluno['code'] === 200) {
+            echo "Nome do aluno: " . $aluno['body']['nome'];
+        } else {
+            echo "Aluno não encontrado.";
+        }
+    }
+
+   /* public function atualizarUsuario($id, $nome, $email)
+    {
+        $sql = "UPDATE usuarios SET nome = :nome, email = :email WHERE id = :id";
+        $query = $this->db->prepare($sql);
+        $query->execute([
+            'nome' => $nome,
+            'email' => $email,
+            'id' => $id
+        ]);
+    } */
+}
---
+++ frontend-php/classes/ApiNodeService.php
@@ -0,0 +1,62 @@
+<?php
+
+class ApiNodeService
+{
+    private string $baseUrl;
+    private string $apiKey;
+
+    public function __construct()
+    {
+        $this->baseUrl = 'http://localhost/api';
+        //$this->apiKey = "reqres-free-v1";
+    }
+
+    private function request(string $endpoint, string $method = 'GET', array $data = []): array
+    {
+        $ch = curl_init($this->baseUrl . $endpoint);
+
+        $headers = [
+            'Content-Type: application/json',
+            'x-api-key: ' . $this->apiKey
+        ];
+        $options = [
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_CUSTOMREQUEST => $method,
+            CURLOPT_HTTPHEADER => $headers,
+        ];
+
+        if (in_array($method, ['POST', 'PUT'])) {
+            $options[CURLOPT_POSTFIELDS] = json_encode($data);
+        }
+
+        curl_setopt_array($ch, $options);
+
+        $response = curl_exec($ch);
+        $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+
+        return [
+            'code' => $httpcode,
+            'body' => json_decode($response, true)
+        ];
+    }
+
+    //Aluno
+    public function buscarAluno(int $id): array
+    {
+        return $this->request("/alunos/{$id}", 'GET');
+    }
+
+    public function cadastrarAluno(array $aluno): array
+    {
+        return $this->request('/aluno', 'POST', $aluno);
+    }
+
+    /*public function atualizarAluno(int $id, array $usuario): array
+    {
+        return $this->request("/aluno/{$id}", 'PUT', $usuario);
+    }*/
+
+    //Eventos
+    
+}
